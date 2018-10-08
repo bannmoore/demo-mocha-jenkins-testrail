f6e20769c9ac559b53879b1f0f43a72671d90fc0
bannmoore <moore.brittanyann@gmail.com>
2018-10-08 11:23:41 -0400

    feat: Install testrail artifacts in an untracked directory.
    Side Effects:
    - Fixup tar logic to remove unnecessary root directory.

--- build-testrail-plugin.sh
+++ build-testrail-plugin.sh
@@ -2,6 +2,14 @@
 set -evuo pipefail
 IFS=$'\n\t'
 
+mkdir temp || true
+cd temp
+
 wget https://github.com/jenkinsci/testrail-plugin/archive/testrail-1.0.6.tar.gz
-tar -zxf testrail-1.0.6.tar.gz
-docker run -it --rm --name testrail-jenkins-build -v "$(pwd)":/usr/src/testrail-jenkins -w /usr/src/testrail-jenkins maven:3.5.3-jdk-8 mvn package
+tar -zxf testrail-1.0.6.tar.gz --strip-components 1
+
+docker run -it --rm --name testrail-jenkins-build -v "$(pwd)":/usr/src/testrail-jenkins \
+  -w /usr/src/testrail-jenkins \
+  maven:3.5.3-jdk-8 mvn clean package
+
+cd ..
